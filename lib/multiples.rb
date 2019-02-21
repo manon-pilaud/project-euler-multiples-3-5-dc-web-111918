e45b65777e2dc23a2ffb3f688fe6cb016a086a79
Manon Pilaud <mandyypilaud@gmail.com>
2019-02-21 09:55:30 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  i=1
+  ar = []
+  while i<limit
+    if i%3 ==0 || i%5 == 0
+      ar.push(i)
+    end
+    i+=1
+  end
+  ar
+end
+
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_reader :limit, :multiples
+
+  def initialize(limit)
+    @limit = limit
+    @multiples = collect_multiples
+  end
+
+  def collect_multiples
+    i=1
+    ar = []
+    while i<limit
+      if i%3 ==0 || i%5 == 0
+        ar.push(i)
+      end
+      i+=1
+    end
+    ar
+  end
+
+  def sum_multiples
+    multiples.inject(:+)
+  end
+end
